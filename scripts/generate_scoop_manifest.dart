85b1ccdf2a8863bf38ffb2f675e4b194a6495d96
Sammy Al Hashemi <sammy@salh.xyz>
2026-02-05 16:21:17 -0500

    fix: make scoop version timestamp more granular (HHMMSS)

--- scripts/generate_scoop_manifest.dart
+++ scripts/generate_scoop_manifest.dart
@@ -10,8 +10,9 @@ void main(List<String> args) {
   if (version == 'nightly') {
     final now = DateTime.now();
     final dateStr = "${now.year}${now.month.toString().padLeft(2, '0')}${now.day.toString().padLeft(2, '0')}";
+    final timeStr = "${now.hour.toString().padLeft(2, '0')}${now.minute.toString().padLeft(2, '0')}${now.second.toString().padLeft(2, '0')}";
     // Use a version that Scoop can parse as newer
-    version = "nightly-$dateStr"; 
+    version = "nightly-$dateStr-$timeStr"; 
   }
 
   final manifest = {
